2faa9e6933833e41bb6b0989108828751f67f588
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-17 16:29:33 +0100

    add authentification with Wix headless

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -2,21 +2,54 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import CardModal from "./CardModal";
+import { useWixClient } from "@/hooks/useWixClient";
+import Cookies from "js-cookie";
 
 const NavIcons = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
+  const pathname = usePathname();
   // Temporary
-  const isLoggedIn = false;
+  // const isLoggedIn = false;
+
+  const wixClient = useWixClient();
+  const isLoggedIn = wixClient.auth.loggedIn();
+
   const handleProfile = () => {
     if (!isLoggedIn) {
       router.push("/login");
+    } else {
+      setIsProfileOpen((prev) => !prev);
     }
-    setIsProfileOpen((prev) => !prev);
+  };
+
+  // AUTH WITH WIX_MANAGED AUTH
+  {
+    /* const wixClient = useWixClient();
+  const login = async () => {
+    const loginRequestData = wixClient.auth.generateOAuthData(
+      "http://localhost:3000"
+    );
+    console.log(loginRequestData);
+
+    localStorage.setItem("OAuthRedirectData", JSON.stringify(loginRequestData));
+    const {authUrl} = await wixClient.auth.getAuthUrl(loginRequestData)
+    window.location.href = authUrl;
+  } */
+  }
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    Cookies.remove("refreshToken");
+    const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+    setIsLoading(false);
+    setIsProfileOpen(false);
+    router.push(logoutUrl);
   };
 
   return (
@@ -28,11 +61,14 @@ const NavIcons = () => {
         height={22}
         className="cursor-pointer"
         onClick={handleProfile}
+        //onClick={login}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/">Profile</Link>
-          <div className="mt-2 cursor-pointer">Logout</div>
+          <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+            {isLoading ? "Logging out" : "Logout"}
+          </div>
         </div>
       )}
       <Image
@@ -50,7 +86,9 @@ const NavIcons = () => {
           height={22}
           onClick={() => setIsCartOpen((prev) => !prev)}
         />
-        <div className="absolute -top-4 -right-4 w-6 h-6 bg-bridget rounded-full text-white text-sm flex items-center justify-center">2</div>
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-bridget rounded-full text-white text-sm flex items-center justify-center">
+          2
+        </div>
       </div>
       {isCartOpen && <CardModal />}
     </div>
