abbee43d6acb047c884c67e4e2784c916294cdfc
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-17 11:20:21 +0100

    add filter options on ProductList Component

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -3,14 +3,14 @@ import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import  DOMPurify  from "isomorphic-dompurify";
+import DOMPurify from "isomorphic-dompurify";
 
 const PRODUCT_PER_PAGE = 20;
 
 const ProductList = async ({
   categoryId,
   limit,
-  searchParams
+  searchParams,
 }: {
   categoryId: string;
   limit?: number;
@@ -18,12 +18,31 @@ const ProductList = async ({
 }) => {
   const wixClient = await wixClientServer();
 
-  const res = await wixClient.products
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith("name", searchParams?.name || "")
     .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find();
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE);
+  //.find();
 
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
@@ -60,11 +79,16 @@ const ProductList = async ({
             </span>
           </div>
           {product.additionalInfoSections && (
-            <div className="text-sm text-gray-500" dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(product.additionalInfoSections?.find(
-              (section: any) => section.title === "shortDesc"
-            )?.description || "")}} 
-            >
-            </div>
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections?.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
           )}
           <button className="rounded-2xl ring-1 ring-bridget w-max text-bridget py-2 px-4 text-xs hover:bg-bridget hover:text-white">
             Add to Cart
